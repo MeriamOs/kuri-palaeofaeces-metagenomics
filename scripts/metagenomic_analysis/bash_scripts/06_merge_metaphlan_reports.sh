e95ce5601526c9eef312cf1e51e179cd1905b75d
Meriam van Os <84883985+MeriamOs@users.noreply.github.com>
2026-06-21 13:51:30 +1200

    Update and rename merge_metaphlan_reports.sh to 06_merge_metaphlan_reports.sh

--- scripts/metagenomic_analysis/bash_scripts/06_merge_metaphlan_reports.sh
+++ scripts/metagenomic_analysis/bash_scripts/06_merge_metaphlan_reports.sh
@@ -1,5 +1,7 @@
 #module load MetaPhlAn/4.1.0-gimkl-2022a-Python-3.10.5
 
+### MERGE SAMPLE TABLE BASED ON SIZE, AND OTHER DATA CATEGORIES ###
+
 merge_metaphlan_tables.py reports_under50bp/*.metaphlan_profile.txt > merged_under50bp_table.txt
 merge_metaphlan_tables.py reports_over50bp/*.metaphlan_profile.txt > merged_over50bp_table.txt
 merge_metaphlan_tables.py reports_negatives/*.metaphlan_profile.txt > merged_negatives_table.txt
