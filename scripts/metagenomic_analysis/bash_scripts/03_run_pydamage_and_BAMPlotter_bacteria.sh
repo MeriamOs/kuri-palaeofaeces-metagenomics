72233616d78242500243094f96bf57be53f84b01
Meriam van Os <84883985+MeriamOs@users.noreply.github.com>
2026-06-21 13:11:01 +1200

    Update 03_run_pydamage_and_BAMPlotter_bacteria.sh

--- scripts/metagenomic_analysis/bash_scripts/03_run_pydamage_and_BAMPlotter_bacteria.sh
+++ scripts/metagenomic_analysis/bash_scripts/03_run_pydamage_and_BAMPlotter_bacteria.sh
@@ -8,6 +8,27 @@
 #SBATCH --output bamplotter.%j.out # CHANGE map1 part each run
 #SBATCH --error bamplotter.%j.err # CHANGE map1 part each runmodule purge 
 
+###################
+## MAPPING STATS ##
+###################
+
+module load SAMtools/1.12-GCC-9.2.0
+
+for ref in $(grep '>' ../../references/bacteria_all.fasta | awk '{print substr($1,2)}'); 
+    do
+        echo "$ref"
+        cat header.txt | sed "s/#rname/$ref/" > ${ref}_stats.txt
+        for sample in MS11669 MS11670 MS11673 MS11674 MS11675 MS11676 MS11677 MS11678 \
+                        MS11679 MS11683 MS11684 MS11686 MS11770 MS11771 MS11774 MS11775;
+            do 
+            samtools coverage ${sample}_maponly.bam | grep $ref | sed "s/${ref}/$sample/" >> ${ref}_stats.txt
+        done
+done
+
+####################
+## ACTIVATE CONDA ##
+####################
+
 module purge
 module load Miniconda3
 
