5863fb36814f3608f9f3d616976068af02d5afce
Meriam van Os <84883985+MeriamOs@users.noreply.github.com>
2026-06-28 19:02:36 +1200

    Update 07_run_sourcepredict.sh

--- scripts/metagenomic_analysis/bash_scripts/07_run_sourcepredict.sh
+++ scripts/metagenomic_analysis/bash_scripts/07_run_sourcepredict.sh
@@ -14,11 +14,15 @@ module load Miniconda3
 source $(conda info --base)/etc/profile.d/conda.sh
 export PYTHONNOUSERSITE=1
 
-conda activate sourcepredict
+#conda create -p /nesi/nobackup/uoo02328/meriam/conda_environments/sourcepredict python=3.10 -c conda-forge -c bioconda sourcepredict
+
+conda activate /nesi/nobackup/uoo02328/meriam/conda_environments/sourcepredict
+
+cd /nesi/nobackup/uoo02328/meriam/coprolite_analysis/04-analysis/sourcepredict
 
 sourcepredict -s sp_sources_september2025.csv -l sp_labels_september2025.csv \
-    -o coproid.kuri.standard2024.june2026.50cutoff.sourcepredict.csv \
-    -e coproid.kuri.standard2024.june2026.50cutoff.embedding.csv -t 8 \
-    kraken_reports/kraken_kuri_june2026_50cutoff.csv 
+    -o coproid.kuri.standard2024.june2026.100cutoff.sourcepredict.csv \
+    -e coproid.kuri.standard2024.june2026.100cutoff.embedding.csv -t 8 \
+    kraken_kuri_june2026_100count_minimizer.csv 
 
 conda deactivate
