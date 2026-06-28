280dc03b97a73358d100879965eeac39ba970c22
Meriam van Os <84883985+MeriamOs@users.noreply.github.com>
2026-06-28 16:18:46 +1200

    Update 03_run_pydamage_and_BAMPlotter_bacteria.sh

--- scripts/metagenomic_analysis/bash_scripts/03_run_pydamage_and_BAMPlotter_bacteria.sh
+++ scripts/metagenomic_analysis/bash_scripts/03_run_pydamage_and_BAMPlotter_bacteria.sh
@@ -53,7 +53,7 @@ for ref in $(grep '>' ../../references/bacteria_all.fasta | awk '{print substr($
         for sample in MS11669 MS11670 MS11673 MS11674 MS11675 MS11676 MS11677 MS11678 \
                         MS11679 MS11683 MS11684 MS11686 MS11770 MS11771 MS11774 MS11775;
             do 
-            samtools coverage ${sample}_maponly.bam | grep $ref | sed "s/${ref}/$sample/" >> ${ref}_stats.txt
+            samtools coverage ${sample}_maponly.bam | grep $ref | sed "s/${ref}/$sample/" >> mapping_stats/${ref}_stats.txt
         done
 done
 
@@ -64,7 +64,7 @@ for ref in $(grep '>' ../../references/bacteria_all.fasta | awk '{print substr($
         for sample in MS11669 MS11670 MS11673 MS11674 MS11675 MS11676 MS11677 MS11678 \
                         MS11679 MS11683 MS11684 MS11686 MS11770 MS11771 MS11774 MS11775;
             do 
-            samtools coverage bam_files/${sample}_maponly_q30.bam | grep $ref | sed "s/${ref}/$sample/" >> ${ref}_q30_stats.txt
+            samtools coverage bam_files/${sample}_maponly_q30.bam | grep $ref | sed "s/${ref}/$sample/" >> mapping_stats/${ref}_q30_stats.txt
         done
 done
 
