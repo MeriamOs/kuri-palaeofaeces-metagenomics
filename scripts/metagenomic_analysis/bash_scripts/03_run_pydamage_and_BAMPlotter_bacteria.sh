0da9d418454313ad2aa9300edf0cba1cef825cd5
Meriam van Os <84883985+MeriamOs@users.noreply.github.com>
2026-06-28 16:34:49 +1200

    Update 03_run_pydamage_and_BAMPlotter_bacteria.sh

--- scripts/metagenomic_analysis/bash_scripts/03_run_pydamage_and_BAMPlotter_bacteria.sh
+++ scripts/metagenomic_analysis/bash_scripts/03_run_pydamage_and_BAMPlotter_bacteria.sh
@@ -60,7 +60,7 @@ done
 for ref in $(grep '>' ../../references/bacteria_all.fasta | awk '{print substr($1,2)}'); 
     do
         echo "$ref"
-        cat header.txt | sed "s/#rname/$ref/" > mapping_stats/${ref}_stats.txt
+        cat header.txt | sed "s/#rname/$ref/" > mapping_stats/${ref}_q30_stats.txt
         for sample in MS11669 MS11670 MS11673 MS11674 MS11675 MS11676 MS11677 MS11678 \
                         MS11679 MS11683 MS11684 MS11686 MS11770 MS11771 MS11774 MS11775;
             do 
