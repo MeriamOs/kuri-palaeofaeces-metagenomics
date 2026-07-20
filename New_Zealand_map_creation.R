e20bbeca807481a5652e15b409ad79207ff3aebf
Meriam van Os <84883985+MeriamOs@users.noreply.github.com>
2026-07-20 20:49:42 +1200

    Create New_Zealand_map_creation.R

---
+++ New_Zealand_map_creation.R
@@ -0,0 +1,25 @@
+# New Zealand map creation for publication 
+
+library(sf)
+library(ggplot2)
+library(ggspatial)
+library(rnaturalearth)
+
+nz <- ne_countries(country = "New Zealand",
+  scale = 10, returnclass = "sf")
+
+ggplot(nz) +
+  geom_sf(fill = "darkolivegreen3", colour = "black", linewidth = 0.2) +
+  annotation_north_arrow(
+    location = "tr",
+    which_north = "true",
+    style = north_arrow_fancy_orienteering) +
+  annotation_scale(location = "br",     
+                   width_hint = 0.25,
+                   text_cex = 1.4) +
+  coord_sf(xlim = c(155, 180),
+            ylim = c(-48, -34)) +
+  theme_bw() +
+  theme(axis.text = element_text(size = 18))
+
+ggsave("pub_map_NZ.png", width = 9, height = 7.5)
