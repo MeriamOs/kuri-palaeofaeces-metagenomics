7a2ac5521ff911a28d35a685a864218ca0472cd6
Meriam van Os <84883985+MeriamOs@users.noreply.github.com>
2026-07-26 19:44:09 +1200

    Update 07_run_sourcepredict.sh

--- scripts/metagenomic_analysis/bash_scripts/07_run_sourcepredict.sh
+++ scripts/metagenomic_analysis/bash_scripts/07_run_sourcepredict.sh
@@ -21,8 +21,8 @@ conda activate /nesi/nobackup/uoo02328/meriam/conda_environments/sourcepredict
 cd /nesi/nobackup/uoo02328/meriam/coprolite_analysis/04-analysis/sourcepredict
 
 sourcepredict -s sp_sources_september2025.csv -l sp_labels_september2025.csv \
-    -o coproid.kuri.standard2024.june2026.100cutoff.sourcepredict.csv \
-    -e coproid.kuri.standard2024.june2026.100cutoff.embedding.csv -t 8 \
-    kraken_kuri_june2026_100count_minimizer.csv 
+    -o coproid.kuri.standard2024.june2026.1000cutoff.sourcepredict.csv \
+    -e coproid.kuri.standard2024.june2026.1000cutoff.embedding.csv -t 8 \
+    kraken_kuri_june2026_1000count_minimizer.csv 
 
 conda deactivate
